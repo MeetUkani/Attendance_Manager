ffff3e4a2af55eac02d0b331829163b7a5a25bc9
MeetUkani <96523875+MeetUkani@users.noreply.github.com>
2022-02-16 19:46:53 +0530

    Database Connection Done

--- app/src/main/java/com/example/attendancemanager/Login_Activity.java
+++ app/src/main/java/com/example/attendancemanager/Login_Activity.java
@@ -1,14 +1,59 @@
 package com.example.attendancemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login_Activity extends AppCompatActivity {
+
+    EditText entered_Username;
+    EditText entered_Password;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        entered_Username = findViewById(R.id.username);
+        entered_Password = findViewById(R.id.password);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("login_credentials");
     }
+
+//    public void login_Clicked(View view) {
+//        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
+//        try {
+//            databaseReference.addValueEventListener(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                    for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+//                        if(entered_Username.getText().toString() == dataSnapshot.getValue()){
+//                            databaseReference.child(entered_Username.toString()).get();
+//                        }
+//
+//
+//                    }
+//                }
+//
+//                @Override
+//                public void onCancelled(@NonNull DatabaseError error) {
+//
+//                }
+//            });
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
 }
