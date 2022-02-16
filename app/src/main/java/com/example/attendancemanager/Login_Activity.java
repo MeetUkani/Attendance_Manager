acf0a21ae125c80ae528c6d7f9f31cd05d17109b
MeetUkani <96523875+MeetUkani@users.noreply.github.com>
2022-02-17 01:20:51 +0530

    Database Connection Done

--- app/src/main/java/com/example/attendancemanager/Login_Activity.java
+++ app/src/main/java/com/example/attendancemanager/Login_Activity.java
@@ -4,11 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,9 +20,10 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Login_Activity extends AppCompatActivity {
 
-    EditText entered_Username;
-    EditText entered_Password;
-    DatabaseReference databaseReference;
+    private EditText entered_Username;
+    private EditText entered_Password;
+    private Button login;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +32,26 @@ public class Login_Activity extends AppCompatActivity {
 
         entered_Username = findViewById(R.id.username);
         entered_Password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("login_credentials");
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = entered_Username.toString();
+                String password = entered_Password.toString();
+
+                if (password.length() < 6) {
+                    Toast.makeText(Login_Activity.this, "Password Length is Too Short", Toast.LENGTH_SHORT).show();
+                } else
+                    auth.signInWithEmailAndPassword(username,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(Login_Activity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            }
+        });
     }
 
-//    public void login_Clicked(View view) {
-//        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
-//        try {
-//            databaseReference.addValueEventListener(new ValueEventListener() {
-//                @Override
-//                public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                    for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
-//                        if(entered_Username.getText().toString() == dataSnapshot.getValue()){
-//                            databaseReference.child(entered_Username.toString()).get();
-//                        }
-//
-//
-//                    }
-//                }
-//
-//                @Override
-//                public void onCancelled(@NonNull DatabaseError error) {
-//
-//                }
-//            });
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//    }
 }
